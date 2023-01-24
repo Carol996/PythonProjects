347f3b1be44eba6f207f5b4ca9ca11684b0a3e78
Carol996 <112996622+Carol996@users.noreply.github.com>
2023-01-23 19:15:22 -0800

    Added Phonebook Sample Files

--- notes/Tkinter.py
+++ notes/Tkinter.py
@@ -8,25 +8,44 @@ class ParentWindow(Frame):
 
         self.master = master
         self.master.resizable(width=True, height=True)#true or false values gives me the ability to resize
-        self.master.geometry('{}x{}'.format(400, 400))
+        self.master.geometry('{}x{}'.format(500, 400))
         self.master.title('Learning Tkinter')
         self.master.config(bg="black")
 
         self.varfName = StringVar()
         self.varlName = StringVar()
 
-        self.lblfName = Label(self.master, text = 'First Name ', font = ('Arial', 16), fg = 'green', bg='black')
-        self.lblfName.grid(row = 0, column = 0)
+        self.lblfName = Label(self.master, text = 'First Name: ', font = ('Arial', 16), fg = 'green', bg='black')
+        self.lblfName.grid(row = 0, column = 0, padx=(30, 0), pady=(30,0))
 
-        self.lbllName = Label(self.master, text = 'Last Name ', font = ('Arial', 16), fg = 'green', bg='black')
-        self.lbllName.grid(row = 0, column = 0)
+        self.lbllName = Label(self.master, text = 'Last Name: ', font = ('Arial', 16), fg = 'green', bg='black')
+        self.lbllName.grid(row = 1, column = 0, padx=(30, 0), pady=(30,0))
 
+        self.lblDisplay = Label(self.master, text='', font=('Helvetica', 14), fg='green', bg='black')
+        self.lblDisplay.grid(row =3, column =1, padx=(30, 0), pady=(30,0))
 
         self.txtfName = Entry(self.master, text = self.varfName, font = ('Courier', 16), fg = 'green', bg = 'black')
-        self.txtfName.grid(row =, column =)
+        self.txtfName.grid(row =0, column =1, padx=(30, 0), pady=(30,0))
 
         self.txtlName = Entry(self.master, text = self.varlName, font = ('Courier', 16), fg = 'green', bg = 'black')
-        self.txtlName.grid(row =, column =)
+        self.txtlName.grid(row =1, column =1, padx=(30, 0), pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text = 'Submit', width = 10, height=2, command=self.submit)
+        self.btnSubmit.grid(row=2, column=1, padx=(30,0), pady=(30,0), sticky=NE)
+
+        self.btnCancel = Button(self.master, text = 'Cancel', width = 10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=2, column=1, padx=(0,90), pady=(30,0), sticky=NE)
+
+    def submit(self):
+        fn = self.varfName.get()
+        ln = self.varlName.get()
+        self.lblDisplay.config(text='Hi {} {}'.format(fn, ln))
+
+    def cancel(self):
+        self.master.destroy()
+
+
+        
 
 
 if __name__ == "__main__":
