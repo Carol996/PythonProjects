cddb041ef87c6ab24d411c912cd8b240de6d84a9
Carol996 <112996622+Carol996@users.noreply.github.com>
2023-01-24 18:28:00 -0800

    Update Pvt and Pr Variables
    Previous update was empty

---
+++ assignments/misc assignments/Pvt_Pr_Var.py
@@ -0,0 +1,17 @@
+class Numbers:
+    def __init__(self):
+        self.__privateEven = 2
+        self._protectedOdd = 3
+
+    def getPrivate(self):
+        print(self.__privateEven)
+
+    def setPrivate(self, newPvt):
+        self.__privateEven = newPvt
+
+obj = Numbers()
+obj.getPrivate()
+obj.setPrivate(12)
+obj.getPrivate()
+obj._protectedOdd = 33
+print(obj._protectedOdd)
