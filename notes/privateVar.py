4f0c5842c2f916538a51cc1bcd0511fdf9e63d4a
Carol996 <112996622+Carol996@users.noreply.github.com>
2023-01-24 18:25:09 -0800

    Merged Docs

--- notes/privateVar.py
+++ notes/privateVar.py
@@ -1,8 +1,9 @@
 
 # private variable to ensure it's not changed by accident
-class Evens:
+class Numbers:
     def __init__(self):
         self.__privateEven = 2
+        self._protectedOdd = 3
 
     def getPrivate(self):
         print(self.__privateEven)
@@ -10,8 +11,11 @@ class Evens:
     def setPrivate(self, newPvt):
         self.__privateEven = newPvt
 
-obj = Evens()
+obj = Numbers()
 obj.getPrivate()
 obj.setPrivate(12)
 obj.getPrivate()
+obj._protectedOdd = 33
+print(obj._protectedOdd)
+
     
